361d4693d88710197444913f21e80b713f51da8c
alamonic <alamonic@depaul.edu>
2021-10-16 15:09:02 -0500

    changes to 'remember me'

--- loginPage.js
+++ loginPage.js
@@ -1,14 +1,15 @@
+
 const loginButton = document.getElementById("loginFormSubmit");
 const loginForm = document.getElementById("loginForm");
 const loginErrorMsg = document.getElementById("loginErrorMsg");
 
 loginButton.addEventListener("click", (input) => {
-        input.preventDefault();
-        const username = loginForm.username.value;
-        const password = loginForm.password.value;
-    }
+    input.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+})
    /*
-   We'll need to change this to be universal for each user
+   We'll need to change this to be universal for each user; should work once database is up
             if (username === "user" && password === " ") {
                 alert("Successful, logging in now");
                 location.reload();
@@ -16,5 +17,5 @@ loginButton.addEventListener("click", (input) => {
                 loginErrorMsg.style.opacity = 1;
             }
         }
-    */
-)
+
+)*/
